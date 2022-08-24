36a5f08305f9ebc71b4ae2b613297ff3d523ae10
kj455 <kaji.ibuki45@gmail.com>
2022-08-24 09:58:26 +0900

    feat: quiz detected displays on popup

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,7 +1,53 @@
-import { useEffect } from "react";
+import { useEffect, useState } from 'react';
+
+async function getCurrentTab(): Promise<chrome.tabs.Tab> {
+  return new Promise(function (resolve, reject) {
+    chrome.tabs.query(
+      {
+        active: true,
+        lastFocusedWindow: true,
+      },
+      function (tabs) {
+        resolve(tabs[0]);
+      },
+    );
+  });
+}
 
 export const Popup = () => {
+  const [isQuiz, setIsQuiz] = useState(false);
+
+  // detect `quiz` in the url
   useEffect(() => {
+    (async () => {
+      const tab = await getCurrentTab();
+      setIsQuiz(tab.url?.includes('quiz') ?? false);
+    })();
   }, []);
-  return <div>Hey!</div>;
+
+  return (
+    <div
+      style={{
+        color: '#2D3748',
+        padding: '8px',
+        backgroundColor: '#F7FAFC',
+      }}
+    >
+      {isQuiz ? (
+        <p>
+          Udemy Quiz was shuffled successfully!{' '}
+          <span role="img" aria-label="tada">
+            🎉
+          </span>
+        </p>
+      ) : (
+        <p>
+          Udemy Quiz was not found...
+          <span role="img" aria-label="sad face">
+            🙁
+          </span>
+        </p>
+      )}
+    </div>
+  );
 };
